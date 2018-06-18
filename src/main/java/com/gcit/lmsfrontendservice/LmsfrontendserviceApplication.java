f0aea01f9e2edcaf189cb53a583a586aeab93d5d
iratusmachina <iratusmachina@DESKTOP-8OQ93TH.(none)>
2018-06-18 11:59:54 -0400

    Initial commit

---
+++ src/main/java/com/gcit/lmsfrontendservice/LmsfrontendserviceApplication.java
@@ -0,0 +1,12 @@
+package com.gcit.lmsfrontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsfrontendserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsfrontendserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gcit/lmsfrontendservice/LmsfrontendserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gcit.lmsfrontendservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LmsfrontendserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
